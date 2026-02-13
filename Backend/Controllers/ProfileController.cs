7cf6ce3d4c0e51a2ffa527dcb361e7d12baf9183
Xwaw <zombiefirerod23@gmail.com>
2026-02-14 00:31:26 +0100

    add upload banner endpoint

--- Backend/Controllers/ProfileController.cs
+++ Backend/Controllers/ProfileController.cs
@@ -35,4 +35,17 @@ public class ProfileController : ControllerBase
 
         return Ok(url);
     }
+    
+    [Authorize]
+    [HttpPost("banner/{userId}")]
+    public async Task<IActionResult> UploadAndSaveBanner(string userId, [FromForm] IFormFile file)
+    {
+        var isOwner = await _profileService.IsProfileOwner(User, userId);
+        if(!isOwner)
+            return StatusCode(403);
+
+        var url = await _profileService.ReplaceUserImageAsync(Guid.Parse(userId), file, FileUsageType.Banner);
+
+        return Ok(url);
+    }
 }
--- Frontend/src/pages/Test.tsx
+++ Frontend/src/pages/Test.tsx
@@ -6,53 +6,9 @@ import axios from "axios";
 export default function Test() {
   const navigate = useNavigate();
 
-  const [file, setFile] = useState<File | null>(null);
-
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <button
-        onClick={async () => {
-          try {
-            const res = await http.post(`/mail/test`, {});
-            console.log("OK:", res.data);
-          } catch (err) {
-            console.error("ERROR:", err);
-          }
-        }}
-      >
-        EMAIL
-      </button>
-
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          if (!file) return;
-
-          const formData = new FormData();
-          formData.append("file", file);
-
-          await http.post(
-  `/profile/avatar/1c524396-ae7d-418a-b599-e8d76dd1fcd0`,
-  formData,
-  {
-    headers: {
-      "Content-Type": "multipart/form-data"
-    }
-  }
-);
-
-        }}
-      >
-        <input
-          type="file"
-          name="file"
-          onChange={(e) => {
-            if (e.target.files) setFile(e.target.files[0]);
-          }}
-        />
-        <button type="submit">Upload</button>
-      </form>
+      
     </div>
   );
 }
