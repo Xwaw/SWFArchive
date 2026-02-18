ad26bc7b570cc4ba74a96368ed7284c8281d6aac
Xwaw <zombiefirerod23@gmail.com>
2026-02-19 00:24:01 +0100

    add isOwner hook

---
+++ Frontend/src/features/profile/hooks/useOwner.tsx
@@ -0,0 +1,19 @@
+import { profileService } from "../services/ProfileService";
+import { useEffect, useState } from "react";
+
+export const useOwner = (userId: string) => {
+  const [isOwner, setIsOwner] = useState(false);
+
+  useEffect(() => {
+    const check = async () => {
+      const result = await profileService.isOwner(userId);
+      setIsOwner(result ?? false);
+    };
+
+    if (userId) {
+      check();
+    }
+  }, [userId]);
+
+  return isOwner;
+};
