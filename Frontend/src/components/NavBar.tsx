e56a53bb91efd3f612c643445e81709db99d032f
Xwaw <zombiefirerod23@gmail.com>
2026-03-15 21:41:35 +0100

    refactor navbar component

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,33 +1,52 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../features/authorization/hooks/UseAuth";
 import ProfileButton from "../features/profile/components/ProfileButton";
+import { useProfile } from "../features/profile/hooks/UseProfile";
 
-interface Components{
-  children?: React.ReactNode
+interface Components {
+  children?: React.ReactNode;
 }
-export default function NavBar( {children } : Components) {
+export default function NavBar({ children }: Components) {
   const { isLogged } = useAuth();
+  const { profile } = useProfile();
   const navigate = useNavigate();
 
   return (
     <div className="w-screen h-15 bg-gray-400 flex">
       <div className="w-full h-full bg-amber-950 p-2">
         <div
-          className="w-40 h-full flex justify-center items-center"
+          className="w-40 h-full flex items-center p-2"
           onClick={() => {
             navigate("/archive");
           }}
         >
-          SWFArchive
+          <p
+            style={{
+              fontSize: 35,
+              color: "white",
+              WebkitTextStroke: "2px red",
+              fontWeight: "bold",
+            }}
+          >
+            SWFArchive
+          </p>
         </div>
       </div>
-      <div className="w-full h-full bg-amber-500 p-2">
-          {children}
-      </div>
+      <div className="w-full h-full bg-amber-500 p-2">{children}</div>
 
       <div className="w-full h-full bg-amber-950 p-2 flex gap-5 justify-end items-center">
         {isLogged ? (
-          <div>
+          <div className="flex items-center gap-5">
+            <p
+              style={{
+                fontSize: 25,
+                fontWeight: "bold",
+                WebkitTextStroke: "1px black",
+                color: "white",
+              }}
+            >
+              {profile?.userName}
+            </p>
             <ProfileButton />
           </div>
         ) : (
--- Frontend/src/features/profile/components/ProfileButton.tsx
+++ Frontend/src/features/profile/components/ProfileButton.tsx
@@ -7,27 +7,56 @@ import { Config } from "../../../Config";
 export default function ProfileButton() {
   const navigate = useNavigate();
 
-  const {profile} = useProfile();
+  const { profile } = useProfile();
 
   return (
     <div className="inline-flex items-center justify-center p-1">
       <DropList
         items={[
-          { text: "Profile", onClick: () => {navigate(`/profile/${profile?.userId}`)} },
-          { text: "Library", onClick: () => {navigate("/library")} },
-          { text: "Friends", onClick: () => {navigate("/friends")} },
-          { text: "Settings", onClick: () => {navigate("/settings")} },
-          { text: "Upload", onClick: () => {navigate("upload")} },
+          {
+            text: "Profile",
+            onClick: () => {
+              navigate(`/profile/${profile?.userId}`);
+            },
+          },
+          {
+            text: "Library",
+            onClick: () => {
+              navigate("/library");
+            },
+          },
+          {
+            text: "Friends",
+            onClick: () => {
+              navigate("/friends");
+            },
+          },
+          {
+            text: "Settings",
+            onClick: () => {
+              navigate("/settings");
+            },
+          },
+          {
+            text: "Upload",
+            onClick: () => {
+              navigate("upload");
+            },
+          },
           {
             text: "Logout",
-            onClick: async () => {await authService.logout()},
+            onClick: async () => {
+              var response = await authService.logout();
+              if(response)
+                window.location.reload();
+            },
           },
         ]}
         button={
           <img
             src={Config.API_URL + profile?.avatarUrl}
             alt={profile?.userName || "Avatar"}
-            className="h-12 w-12 object-cover cursor-pointer"
+            className="h-12 w-12 object-cover cursor-pointer hover:opacity-80"
           />
         }
       />
